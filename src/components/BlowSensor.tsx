3871fafdef511316b1aa39ea89bf8aedf412319c
leeye51456 <leeye51456@gmail.com>
2022-06-14 22:08:58 +0900

    Feat(BlowSensor): Change implementation
    Change implementation to update `lastupdate` state 16 times per a second
    and detect change of this state to calculate a new RMS value.

--- src/components/BlowSensor.tsx
+++ src/components/BlowSensor.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import getRmsFromFloat32Array from '../core/getRmsFromFloat32Array'
 import noOperationFunction from '../core/noOperationFunction'
@@ -6,32 +6,36 @@ import RmsQueue from '../core/RmsQueue'
 import useAudioDevice from '../hooks/useAudioDevice'
 
 type BlowSensorProps = {
-  onAttack?: (...args: unknown[]) => unknown
-  onRelease?: (...args: unknown[]) => unknown
+  onAttack?: (...args: any[]) => unknown
+  onRelease?: (...args: any[]) => unknown
 }
 
-type BlowSensorProgress = 'released' | 'attacked'
+type BlowSensorProgress = 'init' | 'released' | 'attacked' | 'term'
 
 // TODO - Find proper threshold values
 const peakThreshold = 0.97
-const attackThresholdRatio = 1.25
-const releaseThresholdRatio = 0.8
+const attackThresholdRatio = 16
+const releaseThresholdRatio = 8 / attackThresholdRatio
 
 let context: AudioContext | undefined = undefined
 let sourceNode: MediaStreamAudioSourceNode | undefined = undefined
 let analyserNode: AnalyserNode | undefined = undefined
+let destinationNode: MediaStreamAudioDestinationNode | undefined = undefined
 let timeDomainData: Float32Array | undefined = undefined
 let rmsQueue: RmsQueue | undefined = undefined
 
 let intervalId: number | undefined = undefined
 let attackLevel: number = 0
-let progress: BlowSensorProgress = 'released'
+let progress: BlowSensorProgress = 'init'
 
 function BlowSensor({
   onAttack = noOperationFunction,
   onRelease = noOperationFunction,
 }: BlowSensorProps) {
   const stream = useAudioDevice()
+  const [isContextReady, setIsContextReady] = useState(false)
+  const [initTime, setInitTime] = useState(Date.now())
+  const [lastupdate, setLastUpdate] = useState(0)
 
   useEffect(
     () => {
@@ -46,60 +50,37 @@ function BlowSensor({
         { mediaStream: stream }
       )
 
-      analyserNode = new AnalyserNode(context, { fftSize: 4096 })
+      analyserNode = new AnalyserNode(context, {
+        fftSize: 4096,
+        minDecibels: -96,
+        maxDecibels: 0,
+      })
       timeDomainData = new Float32Array(analyserNode.frequencyBinCount)
 
-      sourceNode.connect(analyserNode)
+      destinationNode = new MediaStreamAudioDestinationNode(context)
+
+      sourceNode
+        .connect(analyserNode)
+        .connect(destinationNode)
 
       rmsQueue = new RmsQueue(41)
+
+      setIsContextReady(true)
     },
     [stream]
   )
 
   useEffect(
     () => {
-      if (!context || typeof intervalId === 'number') {
+      if (!isContextReady || !context || typeof intervalId === 'number') {
         return
       }
 
+      progress = 'init'
+      setInitTime(Date.now())
+
       intervalId = window.setInterval(
-        () => {
-          if (
-            !stream ||
-            !context ||
-            !sourceNode ||
-            !analyserNode ||
-            !timeDomainData ||
-            !rmsQueue
-          ) {
-            return
-          }
-
-          analyserNode.getFloatTimeDomainData(timeDomainData)
-          const rms = getRmsFromFloat32Array(timeDomainData)
-          rmsQueue.push(rms)
-
-          if (
-            progress === 'released' &&
-            rms >= Math.min(
-              peakThreshold,
-              rmsQueue.average * attackThresholdRatio
-            )
-          ) {
-            attackLevel = rms
-            progress = 'attacked'
-            onAttack()
-          } else if (
-            progress === 'attacked' &&
-            rms <= Math.min(
-              peakThreshold,
-              attackLevel * releaseThresholdRatio
-            )
-          ) {
-            progress = 'released'
-            onRelease()
-          }
-        },
+        () => setLastUpdate(Date.now()),
         1000 / 16
       )
 
@@ -108,8 +89,46 @@ function BlowSensor({
         intervalId = undefined
       }
     },
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [context]
+    [isContextReady]
+  )
+
+  useEffect(
+    () => {
+      if (
+        initTime > Date.now() - 500 ||
+        !stream ||
+        !context ||
+        !sourceNode ||
+        !analyserNode ||
+        !timeDomainData ||
+        !rmsQueue
+      ) {
+        return
+      }
+
+      // FIXME - Zero data on iPhone series (bypassed?)
+      analyserNode.getFloatTimeDomainData(timeDomainData)
+      const rms = getRmsFromFloat32Array(timeDomainData)
+      rmsQueue.push(rms)
+
+      if (progress === 'init') {
+        progress = 'released'
+      } else if (
+        progress === 'released' &&
+        rms > Math.min(peakThreshold, rmsQueue.average * attackThresholdRatio)
+      ) {
+        attackLevel = rms
+        progress = 'attacked'
+        onAttack({ attackLevel, average: rmsQueue.average })
+      } else if (
+        progress === 'attacked' &&
+        rms <= Math.min(peakThreshold, attackLevel * releaseThresholdRatio)
+      ) {
+        progress = 'released'
+        onRelease({ attackLevel, average: rmsQueue.average })
+      }
+    },
+    [stream, initTime, lastupdate, onAttack, onRelease]
   )
 
   return <></>
